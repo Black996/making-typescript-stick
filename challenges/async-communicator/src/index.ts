dbf40bb937f4816bf11b118df6884d8fbf6ecbc3
black996 <rharasis@yahoo.com>
2022-09-22 00:09:37 +0300

    impl Promisifying methods type challenge (Penpal types challenge)

--- challenges/async-communicator/src/index.ts
+++ challenges/async-communicator/src/index.ts
@@ -1,22 +1,32 @@
 import { expectType } from "tsd";
 
 // IMPLEMENT THIS TYPE
-export type WrapForPenpal<T> = any;
+export type WrapForPenpal<T>= 
+    {[k in keyof T]: T[k] extends (...args:any)=> infer R 
+      ? R extends Promise<any> 
+      ? (...args:any) => R : (...args:any) => Promise<R> 
+      : T[k];
+    };
 
 /**
  * Test Scenario - Do not change anything below this line
  */
 const methods = {
+  foo:'wfewf',
   add(a: number, b: number): number {
     return a + b;
   },
-  subtract(a: number, b: number): number {
-    return a - b;
-  },
+  subtract(a: number, b: number): Promise<number> {
+    return Promise.resolve(a - b);
+  }
 };
+
+// let m: typeof methods
 const asyncMethods: WrapForPenpal<typeof methods> = {} as any;
 
 let addPromise = asyncMethods.add(1, 2);
+let fooPromise = asyncMethods.foo;
+
 expectType<Promise<number>>(addPromise);
 // @ts-expect-error
 expectType<typeof addPromise>("this should fail");
