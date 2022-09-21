f11ff45a61961307d62df85228687f60677014c7
black996 <rharasis@yahoo.com>
2022-09-22 00:06:47 +0300

    impl jquery mock types challenge

--- challenges/jquery-clone/src/index.ts
+++ challenges/jquery-clone/src/index.ts
@@ -1,12 +1,47 @@
-import fetch from "node-fetch";
+import fetch, { RequestInit, Response } from "node-fetch";
 
-function $(selector: string): any {
-  return {} as any;
+class SelectorResult {
+  #element: Element;
+
+  constructor(element: Element){
+    this.#element = element;
+  }
+
+  html(content: string){
+    this.#element.innerHTML = content;
+    return this;
+  }
+
+  hide(){
+    const el = this.#element as HTMLElement;
+    el.style.visibility = 'hidden';
+  }
+
+  show(){
+    const el = this.#element as HTMLElement;
+    el.style.visibility = 'visible';
+  }
+
+  on<T extends keyof HTMLElementEventMap>(eventName: T,
+     eventHandler: (evt: HTMLElementEventMap[T]) => void) {
+    const el = this.#element as HTMLElement;
+    el.addEventListener(eventName, eventHandler);
+  }
+
+
+
+}
+
+function $(selector: string) {
+  const el = document.querySelector(selector) as HTMLElement;
+  return new SelectorResult(el );
 }
 
 namespace $ {
-  export function ajax(...args: any[]): any {
-    return {} as any;
+  export function ajax<T extends Response>(
+    configs: { url: string, successCb: (data: T) => void, options: RequestInit }
+    ): Promise<void> {
+      return fetch(configs.url,configs.options).then((res)=>res.json()).then(configs.successCb);
   }
 }
 
